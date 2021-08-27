f2bd38f25aa087c0a3d8c69b5adf7d98e667efd4
Sonam <sonam.malhotra2496@gmail.com>
2021-08-27 18:49:19 +0000

    Done by searching breed fetcher

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,22 @@
+//const { fetchBreedDescription } = require('./breedFetcher');
 const request = require('request');
-const Input = process.argv;
-const catbreedSearcher = function (){
-  request(`https://api.thecatapi.com/v1/breeds/search?q=${Input[2]}`, (error, response, body) => {
-    if(error){
-      console.log( error); // Print the error if one occurred
-    }
-    else{
-      const catbreedInfo = JSON.parse(body);
-      console.log(catbreedInfo['0']['description']); // Print the HTML for the Google homepage.
-    }
-  });
-}
-catbreedSearcher();
 
+const fetchBreedDescription = function(breedName, callback) {
+ 
+  
+    request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+      if (error) {
+       callback(error, null)
+       return; 
+      } 
 
+         const data = JSON.parse(body); 
+         //console.log(data);
+         callback(null, data[0].description)
+    });
+  
+  
+
+};
+
+module.exports = { fetchBreedDescription };
